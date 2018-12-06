cc31171af2571f7ac2062234661c4ba1b45ff686
Unknown <rachel.mabire@wanadoo.fr>
2018-12-06 19:20:30 +0100

    Modification of the page Visiteurs became available in english and in french

--- src/OC/CoreBundle/Form/TicketType.php
+++ src/OC/CoreBundle/Form/TicketType.php
@@ -6,17 +6,32 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Translation\Translator;
+
 
 class TicketType extends AbstractType
 {
+    private $translator;
+
+    private function getTranslator() {
+
+        if(!isset($this->translator)) {
+            $this->translator = new Translator($this->options['locale']);
+        }
+
+        return $this->translator;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $this->options = $options;
+
         $builder
-        ->add('visitor',     VisitorType::class) 
-        ->add('reducedRate', CheckboxType::class, array('required' => false, 'label' => 'Tarif réduit'));
+        ->add('visitor',     VisitorType::class, array('label' => $this->getTranslator()->trans('label.visitor'))) 
+        ->add('reducedRate', CheckboxType::class, array('required' => false, 'label' => $this->getTranslator()->trans('label.reducedRate')));
     }
         
 
@@ -27,7 +42,8 @@ class TicketType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-           'data_class' => 'OC\CoreBundle\Entity\Ticket'
+           'data_class' => 'OC\CoreBundle\Entity\Ticket',
+           'locale' => 'fr', // the default locale
         ));
     }
 
--- src/OC/CoreBundle/Form/VisitorType.php
+++ src/OC/CoreBundle/Form/VisitorType.php
@@ -8,23 +8,38 @@ use Symfony\Component\Form\Extension\Core\Type\CountryType;
 use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Translation\Translator;
+
 
 class VisitorType extends AbstractType
 {
+    private $translator;
+
+    private function getTranslator() {
+
+        if(!isset($this->translator)) {
+            $this->translator = new Translator($this->options['locale']);
+        }
+
+        return $this->translator;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $this->options = $options;
+
         $builder
-        ->add('visitorName',        TextType::class, array('label' => 'Nom'))
-        ->add('visitorFirstName',   TextType::class, array('label' => 'Prénom'))
+        ->add('visitorName',        TextType::class, array('label' => $this->getTranslator()->trans('label.visitorName')))
+        ->add('visitorFirstName',   TextType::class, array('label'  => $this->getTranslator()->trans('label.visitorFirstName')))
         ->add('birthday',           BirthdayType::class, array(
             'widget' => 'choice',
-            'label' => "Date d'anniversaire",
+            'label' => $this->getTranslator()->trans('label.birthdayDate'),
             'format' => 'dd-MM-yyyy',
         ))
-        ->add('country',        CountryType::class, array('label' => 'Pays'));
+        ->add('country',        CountryType::class, array('label'=> $this->getTranslator()->trans('label.country')));
     }
     
 
@@ -36,7 +51,8 @@ class VisitorType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'OC\CoreBundle\Entity\Visitor'
+            'data_class' => 'OC\CoreBundle\Entity\Visitor',
+            'locale' => 'fr', // the default locale
         ));
     }
 
